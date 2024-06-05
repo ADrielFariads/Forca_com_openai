301326806c5c70a25d28bc8c2ec9045ad5880e0e
adriel <adriel25dias@gmail.com>
2024-06-05 12:07:06 -0300

    adicionei o arquivo estrutural do jogo

---
+++ forca.py
@@ -0,0 +1,57 @@
+"""
+Módulo com as funções básicas do aplicativo
+"""
+
+import os
+
+# Entrada
+palavras_forca = ['adrielson', 'algoritmo', 'programacao']
+
+NUM_MAX_ERRO = 5
+
+
+def escolha_palavra():
+    return palavras_forca[0]
+
+
+def mostre_palavra_terminal(palavra, caracteres_usados):
+    palavra_ao_usuario = ''
+    for letra in palavra:
+        if letra in caracteres_usados:
+            palavra_ao_usuario += letra
+            palavra_ao_usuario += ' '
+        else:
+            palavra_ao_usuario += '- '
+    print(f'Palavra: {palavra_ao_usuario}')
+
+
+def mostre_erros_restantes(NUM_MAX_ERRO, num_erro):
+    print(f'Número de erros disponíveis: {NUM_MAX_ERRO - num_erro}')
+
+
+def mostre_estado_atual(palavra, caracteres_usados, num_erro, NUM_MAX_ERRO):
+    os.system('cls')
+    mostre_palavra_terminal(palavra, caracteres_usados)
+    print(f'Letras já utilizadas: {caracteres_usados}')
+    mostre_erros_restantes(NUM_MAX_ERRO, num_erro)
+
+
+def mensagem_de_vitoria():
+    print()
+    print('Você ganhou!')
+    print(f'A palavra era: {palavra}')
+    print()
+    exit()
+
+
+def mensagem_de_derrota():
+    print()
+    print('Você perdeu!')
+    print(f'A palavra era: {palavra}')
+    print()
+    exit()
+
+
+palavra = escolha_palavra()
+caracteres_usados = []
+num_erro = 0
--- key.py
+++ key.py
@@ -1,1 +1,5 @@
+"""
+Módulo com a chave openai *** Conteúdo sensível e sujeito à perca de validade sem avisos prévios! ***
+"""
+
 OPENAI_API_KEY = "sk-proj-wcv5ihSNXnPmI79EW3vTT3BlbkFJPrQvpOdRpiZEl4sU6r5I"
