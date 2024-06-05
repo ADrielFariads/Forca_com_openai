7915f0e9b8bc8da9a6d36f7b60b797fe04da94ab
Gabriel schuenker <gabrielschuenker@outlook.com>
2024-06-05 19:09:42 -0300

    Coloquei input na chave OpenAI

--- key.py
+++ key.py
@@ -2,4 +2,4 @@
 Módulo com a chave openai *** Conteúdo sensível e sujeito à perca de validade sem avisos prévios! ***
 """
 
-OPENAI_API_KEY = "sk-proj-wcv5ihSNXnPmI79EW3vTT3BlbkFJPrQvpOdRpiZEl4sU6r5I"
+OPENAI_API_KEY = input('Insira sua chave openAI para continuar: ')
